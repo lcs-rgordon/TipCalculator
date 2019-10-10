f2f220b5d1aecf9f43613ac199a0c83c54c248d3
Russell Gordon <github@russellgordon.ca>
2019-10-10 16:35:10 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Gordon, Russell on 2019-10-10.
+//  Copyright © 2019 Gordon, Russell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
