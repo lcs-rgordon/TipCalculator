85bab51ab5748eb3bb4274a16a1abf4447291e74
Russell Gordon <rgordon@lcs.on.ca>
2019-10-10 18:55:38 -0400

    Made outlets and actions to connect view to controller.

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,11 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    // (outlets, variables, constants to be used anywhere below)
+    @IBOutlet weak var billAmount: UITextField!
+    
+    @IBOutlet weak var tipPercentage: UITextField!
+    
+    @IBOutlet weak var peopleCount: UITextField!
+    
+    @IBOutlet weak var totalTipInDollars: UILabel!
+    
+    @IBOutlet weak var tipPerPersonInDollars: UILabel!
+    
+    // MARK: Methods
+    // (functions, actions)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // Calculate the tip and report results
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        
+    }
+    
 
 
 }
